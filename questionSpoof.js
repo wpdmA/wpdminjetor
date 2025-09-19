b30cb8eedfa13f50e925dfbf3958291789382520
WeLL <99270939+wpdmA@users.noreply.github.com>
2025-09-19 16:14:23 -0300

    Update questionSpoof.js

--- questionSpoof.js
+++ questionSpoof.js
@@ -1,9 +1,5 @@
 const phrases = [ 
-    "🔥 Get good, get [Khanware](https://github.com/Niximkk/khanware/)!",
-    "🤍 Made by [@im.nix](https://e-z.bio/sounix).",
-    "☄️ By [Niximkk/khanware](https://github.com/Niximkk/khanware/).",
-    "🌟 Star the project on [GitHub](https://github.com/Niximkk/khanware/)!",
-    "🦢 Nix fez a boa ;)",
+    "WeLL ⛄️",
 ];
 
 const originalFetch = window.fetch;
@@ -26,10 +22,10 @@ window.fetch = async function (input, init) {
                 itemData.question.content = phrases[Math.floor(Math.random() * phrases.length)] + `[[☃ radio 1]]`;
                 itemData.question.widgets = { "radio 1": { type: "radio",  options: { choices: [ { content: "Resposta correta.", correct: true }, { content: "Resposta incorreta.", correct: false } ] } } };
                 responseObj.data.assessmentItem.item.itemData = JSON.stringify(itemData);
-                sendToast("🔓 Questão exploitada.", 1000);
+                sendToast("WeLL ⛄️", 1000);
                 return new Response(JSON.stringify(responseObj), { status: originalResponse.status, statusText: originalResponse.statusText, headers: originalResponse.headers });
             }
         }
-    } catch (e) { debug(`🚨 Error @ questionSpoof.js\n${e}`); }
+    } catch (e) { debug(`🚨 Error @ \n${e}`); }
     return originalResponse;
 };
