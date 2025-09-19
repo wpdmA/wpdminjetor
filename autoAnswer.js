a3af230f0914943237c5b422f447c4fe05951470
WeLL <99270939+wpdmA@users.noreply.github.com>
2025-09-19 16:13:18 -0300

    Update autoAnswer.js

--- autoAnswer.js
+++ autoAnswer.js
@@ -20,7 +20,7 @@ khanwareDominates = true;
             for (const q of selectorsToCheck) {
                 findAndClickBySelector(q);
                 if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
-                    sendToast("🎉 Exercício concluído!", 3000);
+                    sendToast("WeLL ⛄️", 3000);
                     playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
                 }
             }
